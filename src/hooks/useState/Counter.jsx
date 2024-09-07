68cf4ccc6bf7bda513568afaa5b136fcf3ad7adb
Hasarangakgnp <nimna.21@cse.mrt.ac.lk>
2024-09-07 21:46:30 +0530

    useStete

---
+++ src/hooks/useState/Counter.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+const UseState = () => {
+
+    const [counter, setCounter] = useState(0);
+    const [inputValue, setInputValue] = useState('Enter Your name')
+
+    const increment = ()=>{
+        setCounter(counter+1);
+    } 
+    const decrement = ()=>{
+        setCounter(counter-1);
+    } 
+    const refresh = ()=>{
+        setCounter(0);
+    } 
+    const handleInputValue = (event)=>{
+      setInputValue(event.target.value)
+    }
+
+  return (
+    <>
+    <div>
+      Count: {counter}
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={refresh}>Refresh</button>
+    </div>
+    <div>
+      <input type="text" onChange={handleInputValue} />
+      <h1>hello {inputValue}</h1>
+    </div>
+    </>
+  )
+}
+
+export default UseState
